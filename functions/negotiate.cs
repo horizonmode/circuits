a00948fa73f81a674a01052b060506803494d9bf
Seb Smith <sebastian.smith@omm.agency>
2023-07-05 16:12:20 +0100

    add configurable timer params with table storage

--- functions/negotiate.cs
+++ functions/negotiate.cs
@@ -1,22 +1,19 @@
 using System;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Azure;
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
 
 namespace CSharp
 {
     public static class Function
     {
-        private static int ActiveTime = 10;
-        private static int RestTime = 5;
-
         [FunctionName("index")]
         public static IActionResult GetHomePage([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req, ExecutionContext context)
         {
@@ -33,17 +30,25 @@ namespace CSharp
 
         [FunctionName("broadcast")]
         public static async Task Broadcast([TimerTrigger("* * * * * *")] TimerInfo myTimer,
-        [SignalR(HubName = "serverless_dev")] IAsyncCollector<SignalRMessage> signalRMessages)
+        [SignalR(HubName = "serverless_dev")] IAsyncCollector<SignalRMessage> signalRMessages,
+         [Table("Workouts", "Active")] TableClient tableClient, ILogger log)
         {
-            if (ActiveTime <= 0 && RestTime <= 0)
+            var workout = tableClient.Query<ActiveWorkout>(filter: $"PartitionKey eq 'Active'", maxPerPage: 10).FirstOrDefault();
+
+            if (workout == null) return;
+
+            log.LogInformation($"PK={workout.PartitionKey}, RK={workout.RowKey}, ActiveTime={workout.CurrentActiveTime}");
+
+            if (workout.CurrentActiveTime <= 0 && workout.CurrentRestTime <= 0)
             {
-                ActiveTime = 10;
-                RestTime = 5;
+                workout.CurrentActiveTime = workout.ActiveTime;
+                workout.CurrentRestTime = workout.RestTime;
             }
 
-            var mode = ActiveTime > 0 ? "active" : "rest";
-            var timeLeft = ActiveTime <= 0 ? RestTime-- : ActiveTime--;
+            var mode = workout.CurrentActiveTime > 0 ? "active" : "rest";
+            var timeLeft = workout.CurrentActiveTime <= 0 ? workout.CurrentRestTime-- : workout.CurrentActiveTime--;
 
+            tableClient.UpsertEntity<ActiveWorkout>(workout);
 
             await signalRMessages.AddAsync(
                 new SignalRMessage
@@ -51,6 +56,43 @@ namespace CSharp
                     Target = "newMessage",
                     Arguments = new[] { $"{timeLeft}", $"{mode}" }
                 });
+        }
+
+        [FunctionName("ReadActiveWorkout")]
+        public static void ReadActiveWorkout([QueueTrigger("active-workout")] string input,
+        [Table("Workouts", "Active", "{queueTrigger}")] ActiveWorkout workout,
+        ILogger log)
+        {
+            log.LogInformation($"PK={workout.PartitionKey}, RK={workout.RowKey}, ActiveTime={workout.ActiveTime}");
+        }
+
+        [FunctionName("SetActiveWorkout")]
+        [return: Queue("active-workout")]
+        public static string SetActiveWorkout([HttpTrigger] HttpRequest req,
+         [Table("Workouts", Connection = "AzureWebJobsStorage")] TableClient workoutTable, ILogger log)
+        {
+            log.LogInformation($"C# http trigger function processed");
+            int activeTime = int.Parse(req.Query["activeTime"]);
+            int restTime = int.Parse(req.Query["restTime"]);
+
+            string name = req.Query["name"];
+            var workout = new ActiveWorkout { PartitionKey = "Active", RowKey = "HFC", ActiveTime = activeTime, RestTime = restTime };
+            workoutTable.UpsertEntity<ActiveWorkout>(workout);
+
+            return workout.RowKey;
+        }
+
+
+        public class ActiveWorkout : Azure.Data.Tables.ITableEntity
+        {
+            public string PartitionKey { get; set; }
+            public string RowKey { get; set; }
+            public DateTimeOffset? Timestamp { get; set; }
+            public ETag ETag { get; set; }
+            public int ActiveTime { get; set; }
+            public int RestTime { get; set; }
+            public int CurrentActiveTime { get; set; }
+            public int CurrentRestTime { get; set; }
         }
     }
 }
