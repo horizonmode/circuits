fe011c077952a346bf2a8657e2191918b4cbae14
Seb Smith <sebastian.smith@omm.agency>
2023-07-11 10:41:09 +0100

    add dropdown

--- src/app/admin/programmes/edit/page.tsx
+++ src/app/admin/programmes/edit/page.tsx
@@ -1,10 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
+import { LoginForm } from "@/components/login";
+
+const styles = {
+  label: "block text-gray-700 text-sm font-bold pt-2 pb-1",
+  field:
+    "bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none",
+  button:
+    " bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600",
+  errorMsg: "text-red-500 text-sm",
+};
 
 export default function EditProgramme() {
   return (
     <main className="flex flex-col items-left align-left ">
-      <div className="flex flex-col gap-5 "></div>
+      <LoginForm styles={styles} />
     </main>
   );
 }
---
+++ src/components/login.tsx
@@ -0,0 +1,42 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import FileUploadSingle from "./upload";
+import DropDown from "./dropdown";
+//import { loginSchema } from './validation/loginSchema'
+
+export const LoginForm = ({ styles }: any) => (
+  <>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      // validationSchema={loginSchema}
+      onSubmit={(values) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <label className={styles.label} htmlFor="Email">
+          Email
+        </label>
+        <Field className={styles.field} id="email" name="email" />
+        <ErrorMessage component="a" className={styles.errorMsg} name="email" />
+        <label className={styles.label} htmlFor="Email">
+          Password
+        </label>
+        <Field className={styles.field} id="password" name="password" />
+        <ErrorMessage
+          component="a"
+          className={styles.errorMsg}
+          name="password"
+        />
+        <div className="mt-8">
+          <button type="submit" className={styles.button}>
+            Login
+          </button>
+        </div>
+        <DropDown />
+      </Form>
+    </Formik>
+  </>
+);
