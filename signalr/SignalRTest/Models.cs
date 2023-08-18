58a212ec145af77454a0a8bf99d3bcdd9d215739
Seb Smith <sebastian.smith@omm.agency>
2023-08-18 09:43:22 +0100

    dsds

---
+++ signalr/SignalRTest/Models.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SignalRTest
+{
+    public class ActiveProgramme : Programme
+    {
+        public int CurrentActiveTime { get; set; }
+        public int CurrentRestTime { get; set; }
+        public string SourceWorkoutId { get; set; }
+        public bool IsPlaying { get; set; }
+    }
+
+    public class Screen : Dto
+    {
+        public string Tag { get; set; }
+        public string DeviceId { get; set; }
+    }
+
+    public class Programme : Dto
+    {
+        public string Name { get; set; }
+        public int ActiveTime { get; set; }
+        public int RestTime { get; set; }
+        public string Message { get; set; }
+        public List<ScreenMapping> Mappings { get; set; } = new List<ScreenMapping>();
+
+        public Programme Copy()
+        {
+
+            Programme duplicate = (Programme)this.MemberwiseClone();
+            duplicate.id = Guid.NewGuid().ToString();
+            duplicate.Name = $"{this.Name}_copy";
+            return duplicate;
+        }
+    }
+
+    public class ScreenMapping
+    {
+        public Screen Screen { get; set; }
+        public bool SplitScreen { get; set; }
+        public Exercise Exercise1 { get; set; }
+        public Exercise Exercise2 { get; set; }
+        public string ScreenTitle1 { get; set; }
+        public string ScreenTitle2 { get; set; }
+        public bool ShowTimer { get; set; }
+    }
+
+    public class Exercise : Dto
+    {
+        public string Name { get; set; }
+        public string VideoUrl { get; set; }
+        public string VideoFileName { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class Dto
+    {
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
+    }
+}
